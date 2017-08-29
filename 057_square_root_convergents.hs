29f61bc8dcf4d20fad426b32dacf516183afbd56
FG-TUM <f.gratl@tum.de>
2017-08-29 16:08:53 +0200

    solution 57

---
+++ 057_square_root_convergents.hs
@@ -0,0 +1,28 @@
+-- It is possible to show that the square root of two can be expressed as an infinite continued fraction.
+
+-- √ 2 = 1 + 1/(2 + 1/(2 + 1/(2 + ... ))) = 1.414213...
+
+-- By expanding this for the first four iterations, we get:
+
+-- 1 + 1/2 = 3/2 = 1.5
+-- 1 + 1/(2 + 1/2) = 7/5 = 1.4
+-- 1 + 1/(2 + 1/(2 + 1/2)) = 17/12 = 1.41666...
+-- 1 + 1/(2 + 1/(2 + 1/(2 + 1/2))) = 41/29 = 1.41379...
+
+-- The next three expansions are 99/70, 239/169, and 577/408, but the eighth expansion, 1393/985, is the first example where the number of digits in the numerator exceeds the number of digits in the denominator.
+
+-- In the first one-thousand expansions, how many fractions contain a numerator with more digits than denominator?
+
+import Data.Ratio
+import Numeric
+
+import MyLib_haskell
+
+numeratorGTdenum :: Rational -> Bool
+numeratorGTdenum n = numLength (numerator n) > numLength (denominator n)
+
+iterativeRoot :: Int -> [Rational]
+iterativeRoot n = map (+ (-1)) $ take n $ tail $ iterate (\x -> 2 + 1/x) 2
+
+main :: IO()
+main = print . length . filter (== True) . map numeratorGTdenum . iterativeRoot $ 1000

df7249933cc8c209f1d87c4e515e983a9f9ca4b1
FG-TUM <f.gratl@tum.de>
2017-08-29 14:46:43 +0200

    solution 56

---
+++ 056_powerful_digit_sum.hs
@@ -0,0 +1,8 @@
+-- A googol (10^100) is a massive number: one followed by one-hundred zeros; 100^100 is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+
+-- Considering natural numbers of the form, a^b, where a, b < 100, what is the maximum digital sum?
+
+import MyLib_haskell
+
+main :: IO()
+main = print $ maximum $ map digitSum [a^b | a <- [1..100], b <- [1..100]]
